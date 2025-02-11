0fbd9bf511825046e59e559966ec3597be9f32f4
TanaratSudjai <650112418012@bru.ac.th>
2025-02-11 14:18:19 +0700

    loginpage-process

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login_auth');
 });
 Route::get('/about', function () {
     return view('about');
