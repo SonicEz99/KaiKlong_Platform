603522b97fc69f581f290854ae8461dacf1f1c76
ZonixEz <pnem2003@gmail.com>
2025-02-28 16:57:02 +0700

    crud_favorite_done

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -10,4 +10,20 @@ class Favorite extends Model
     use HasFactory;
 
     protected $primaryKey = 'favorite_id';
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
 }
