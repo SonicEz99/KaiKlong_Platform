11584fe554d8af3cb353da46bd5e8922ec2a5706
Monolixd <jakkridjantung@gmail.com>
2025-03-03 11:59:27 +0700

    update font

--- resources/views/massage/chatpage.blade.php
+++ resources/views/massage/chatpage.blade.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>แชทสนทนากับผู้ขาย</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
     <style>
         body {
-            font-family: Arial, sans-serif;
             background-color: #f3f3f3;
             margin: 0;
             padding: 0;
@@ -123,7 +125,7 @@
 
 @section('content')
 
-    <body>
+    <body style="font-family: 'Noto Sans Thai', 'Prompt', sans-serif;">
         <div class="container mt-2">
             <div class="profile_chat">
                 <p>ผู้ขาย</p>
